d5274b4d15346bb3662bf00a9524b839a693b9dc
Gavin Chen <838045868@qq.com>
2020-08-21 16:29:32 +0800

    ss

--- lcd/lcd.c
+++ lcd/lcd.c
@@ -1,61 +1,58 @@
 #include "main.h"
 #include "ascii.h"
 
-#define GPF0CON			(*(volatile unsigned long *)0xE0200120)
-#define GPF1CON			(*(volatile unsigned long *)0xE0200140)
-#define GPF2CON			(*(volatile unsigned long *)0xE0200160)
-#define GPF3CON			(*(volatile unsigned long *)0xE0200180)
+#define GPF0CON (*(volatile unsigned long *)0xE0200120)
+#define GPF1CON (*(volatile unsigned long *)0xE0200140)
+#define GPF2CON (*(volatile unsigned long *)0xE0200160)
+#define GPF3CON (*(volatile unsigned long *)0xE0200180)
 
-#define GPD0CON			(*(volatile unsigned long *)0xE02000A0)
-#define GPD0DAT			(*(volatile unsigned long *)0xE02000A4)
+#define GPD0CON (*(volatile unsigned long *)0xE02000A0)
+#define GPD0DAT (*(volatile unsigned long *)0xE02000A4)
 
-#define CLK_SRC1		(*(volatile unsigned long *)0xe0100204)
-#define CLK_DIV1		(*(volatile unsigned long *)0xe0100304)
-#define DISPLAY_CONTROL	(*(volatile unsigned long *)0xe0107008)
+#define CLK_SRC1 (*(volatile unsigned long *)0xe0100204)
+#define CLK_DIV1 (*(volatile unsigned long *)0xe0100304)
+#define DISPLAY_CONTROL (*(volatile unsigned long *)0xe0107008)
 
-#define VIDCON0			(*(volatile unsigned long *)0xF8000000)
-#define VIDCON1			(*(volatile unsigned long *)0xF8000004)
-#define VIDTCON2		(*(volatile unsigned long *)0xF8000018)
-#define WINCON0 		(*(volatile unsigned long *)0xF8000020)
-#define WINCON2 		(*(volatile unsigned long *)0xF8000028)
-#define SHADOWCON 		(*(volatile unsigned long *)0xF8000034)
-#define VIDOSD0A 		(*(volatile unsigned long *)0xF8000040)
-#define VIDOSD0B 		(*(volatile unsigned long *)0xF8000044)
-#define VIDOSD0C 		(*(volatile unsigned long *)0xF8000048)
+#define VIDCON0 (*(volatile unsigned long *)0xF8000000)
+#define VIDCON1 (*(volatile unsigned long *)0xF8000004)
+#define VIDTCON2 (*(volatile unsigned long *)0xF8000018)
+#define WINCON0 (*(volatile unsigned long *)0xF8000020)
+#define WINCON2 (*(volatile unsigned long *)0xF8000028)
+#define SHADOWCON (*(volatile unsigned long *)0xF8000034)
+#define VIDOSD0A (*(volatile unsigned long *)0xF8000040)
+#define VIDOSD0B (*(volatile unsigned long *)0xF8000044)
+#define VIDOSD0C (*(volatile unsigned long *)0xF8000048)
 
-#define VIDW00ADD0B0 	(*(volatile unsigned long *)0xF80000A0)
-#define VIDW00ADD1B0 	(*(volatile unsigned long *)0xF80000D0)
+#define VIDW00ADD0B0 (*(volatile unsigned long *)0xF80000A0)
+#define VIDW00ADD1B0 (*(volatile unsigned long *)0xF80000D0)
 
-#define VIDTCON0 		(*(volatile unsigned long *)0xF8000010)
-#define VIDTCON1 		(*(volatile unsigned long *)0xF8000014)
+#define VIDTCON0 (*(volatile unsigned long *)0xF8000010)
+#define VIDTCON1 (*(volatile unsigned long *)0xF8000014)
 
-#define HSPW 			(20)				// 1~40 DCLK
-#define HBPD			(46)			// 46
-#define HFPD 			(240 - 1)			// 16 210 354
-#define VSPW			(20)				// 1~20 DCLK
-#define VBPD 			(23)			// 23
-#define VFPD 			(130 - 1)			// 7 22 147
-
-
+#define HSPW (20)      // 1~40 DCLK
+#define HBPD (46)      // 46
+#define HFPD (240 - 1) // 16 210 354
+#define VSPW (20)      // 1~20 DCLK
+#define VBPD (23)      // 23
+#define VFPD (130 - 1) // 7 22 147
 
 // FB地址
-#define FB_ADDR			(0x25500000)
-#define ROW				(480)
-#define COL				(800)
-#define HOZVAL			(COL-1)
-#define LINEVAL			(ROW-1)
+#define FB_ADDR (0x25500000)
+#define ROW (480)
+#define COL (800)
+#define HOZVAL (COL - 1)
+#define LINEVAL (ROW - 1)
 
-#define XSIZE			COL
-#define YSIZE			ROW
+#define XSIZE COL
+#define YSIZE ROW
 
 u32 *pfb = (u32 *)FB_ADDR;
 
-
 // 常用颜色定义
-#define BLUE	0x0000FF
-#define RED		0xFF0000
-#define GREEN	0x00FF00
-#define WHITE	0xFFFFFF
+#define BLUE 0x0000FF
+#define RED 0xFF0000
+#define GREEN 0x00FF00
+#define WHITE 0xFFFFFF
 
 // 初始化LCD
 void lcd_init(void)
@@ -67,59 +64,57 @@ void lcd_init(void)
     GPF3CON = 0x22222222;
 
     // 打开背光 GPD0_0（PWMTOUT0）
-    GPD0CON &= ~(0xf<<0);
-    GPD0CON |= (1<<0);          // output mode
-    GPD0DAT &= ~(1<<0);            // output 0 to enable backlight
+    GPD0CON &= ~(0xf << 0);
+    GPD0CON |= (1 << 0);  // output mode
+    GPD0DAT &= ~(1 << 0); // output 0 to enable backlight
 
     // 10: RGB=FIMD I80=FIMD ITU=FIMD
-    DISPLAY_CONTROL = 2<<0;
+    DISPLAY_CONTROL = 2 << 0;
 
     // bit[26~28]:使用RGB接口
     // bit[18]:RGB 并行
     // bit[2]:选择时钟源为HCLK_DSYS=166MHz
-    VIDCON0 &= ~( (3<<26)|(1<<18)|(1<<2) );
+    VIDCON0 &= ~((3 << 26) | (1 << 18) | (1 << 2));
 
     // bit[1]:使能lcd控制器
     // bit[0]:当前帧结束后使能lcd控制器
-    VIDCON0 |= ( (1<<0)|(1<<1) );
+    VIDCON0 |= ((1 << 0) | (1 << 1));
 
     // bit[6]:选择需要分频
     // bit[6~13]:分频系数为5，即VCLK = 166M/(4+1) = 33M
-    VIDCON0 |= 4<<6 | 1<<4;
-
+    VIDCON0 |= 4 << 6 | 1 << 4;
 
     // H43-HSD043I9W1.pdf(p13) 时序图：VSYNC和HSYNC都是低脉冲
     // s5pv210芯片手册(p1207) 时序图：VSYNC和HSYNC都是高脉冲有效，所以需要反转
-    VIDCON1 |= 1<<5 | 1<<6;
+    VIDCON1 |= 1 << 5 | 1 << 6;
 
     // 设置时序
-    VIDTCON0 = VBPD<<16 | VFPD<<8 | VSPW<<0;
-    VIDTCON1 = HBPD<<16 | HFPD<<8 | HSPW<<0;
+    VIDTCON0 = VBPD << 16 | VFPD << 8 | VSPW << 0;
+    VIDTCON1 = HBPD << 16 | HFPD << 8 | HSPW << 0;
     // 设置长宽(物理屏幕)
     VIDTCON2 = (LINEVAL << 11) | (HOZVAL << 0);
 
     // 设置window0
     // bit[0]:使能
     // bit[2~5]:24bpp（RGB888）
-    WINCON0 |= 1<<0;
+    WINCON0 |= 1 << 0;
     WINCON0 &= ~(0xf << 2);
-    WINCON0 |= (0xB<<2) | (1<<15);
+    WINCON0 |= (0xB << 2) | (1 << 15);
 
-#define LeftTopX     0
-#define LeftTopY     0
-#define RightBotX   799
-#define RightBotY   479
+#define LeftTopX 0
+#define LeftTopY 0
+#define RightBotX 799
+#define RightBotY 479
 
     // 设置window0的上下左右
     // 设置的是显存空间的大小
-    VIDOSD0A = (LeftTopX<<11) | (LeftTopY << 0);
-    VIDOSD0B = (RightBotX<<11) | (RightBotY << 0);
+    VIDOSD0A = (LeftTopX << 11) | (LeftTopY << 0);
+    VIDOSD0B = (RightBotX << 11) | (RightBotY << 0);
     VIDOSD0C = (LINEVAL + 1) * (HOZVAL + 1);
-
 
     // 设置fb的地址
     VIDW00ADD0B0 = FB_ADDR;
-    VIDW00ADD1B0 = (((HOZVAL + 1)*4 + 0) * (LINEVAL + 1));
+    VIDW00ADD1B0 = (((HOZVAL + 1) * 4 + 0) * (LINEVAL + 1));
 
     // 使能channel 0传输数据
     SHADOWCON = 0x1;
@@ -136,24 +131,22 @@ static void lcd_draw_background(u32 color)
 {
     u32 i, j;
 
-    for (j=0; j<COL; j++)
+    for (j = 0; j < COL; j++)
     {
-        for (i=0; i<ROW; i++)
+        for (i = 0; i < ROW; i++)
         {
-            lcd_draw_pixel( j, i,color);
+            lcd_draw_pixel(j, i, color);
         }
     }
 }
 
-
 static void delay(void)
 {
     volatile u32 i, j;
-    for (i=0; i<4000; i++)
-        for (j=0; j<1000; j++);
+    for (i = 0; i < 4000; i++)
+        for (j = 0; j < 1000; j++)
+            ;
 }
-
-
 
 void lcd_test(void)
 {
@@ -171,4 +164,3 @@ void lcd_test(void)
         delay();
     }
 }
-
